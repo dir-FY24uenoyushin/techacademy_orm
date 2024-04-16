2b91d96d29c4cbe33fcb04257146a320dda11ae5
dir-FY24uenoyushin <yushin.ueno.x100@dirbato.co.jp>
2024-04-16 16:23:53 +0900

    Hibernateを使ってDBからデータを取得する

---
+++ techacademy_orm/src/util/DBUtil.java
@@ -0,0 +1,23 @@
+package util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+    private static final String PERSISTENCE_UNIT_NAME = "techacademy_orm";
+    //PERSISTENCE_UNIT_NAME =この後のプロジェクト名を変えれば流用可能
+    private static EntityManagerFactory emf;
+
+    public static EntityManager createEntityManager() {
+        return getEntityManagerFactory().createEntityManager();
+    }
+
+    private static EntityManagerFactory getEntityManagerFactory() {
+        if(emf == null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+
+        return emf;
+    }
+}
